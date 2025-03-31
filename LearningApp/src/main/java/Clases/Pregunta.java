434b21ce538ef5dccf890dddf61c9d478c2ec20a
Jorge <jorge.fernandezc@um.es>
2025-03-31 19:27:15 +0200

    Hay que decirle a JSON qué clase usar dependiendo de cada tipo pregunta.

--- LearningApp/src/main/java/Clases/Pregunta.java
+++ LearningApp/src/main/java/Clases/Pregunta.java
@@ -1,5 +1,19 @@
 package Clases;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+	    use = JsonTypeInfo.Id.NAME,
+	    include = JsonTypeInfo.As.PROPERTY,
+	    property = "tipo"
+)
+@JsonSubTypes({
+	    @JsonSubTypes.Type(value = PreguntaTest.class, name = "test"),
+	    @JsonSubTypes.Type(value = PreguntaRellenarHuecos.class, name = "huecos"),
+	    @JsonSubTypes.Type(value = PreguntaTraduccion.class, name = "traduccion")
+})
+
 public abstract class Pregunta {
     protected String enunciado;
 
