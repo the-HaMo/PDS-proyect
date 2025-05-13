5c9267445a9b247996d9898f023d7b42911d876d
M.Amrou <ma.labiednasser@um.es>
2025-05-13 23:48:35 +0200

    Test de integración y unitarios estructura

---
+++ LearningApp/src/test/java/Controlador/ControladorTestIntegracion.java
@@ -0,0 +1,14 @@
+package Controlador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControladorTestIntegracion {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ LearningApp/src/test/java/Controlador/ControladorTestUnitarias.java
@@ -0,0 +1,63 @@
+package Controlador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import Modelo.BloqueContenido;
+import Modelo.Curso;
+
+class ControladorTestUnitarias {
+
+	@Test
+	void testCursosEnOrdenLikes() {
+		
+		BloqueContenido b1 = new BloqueContenido();
+		BloqueContenido b2 = new BloqueContenido();
+		BloqueContenido b3 = new BloqueContenido();
+		List<BloqueContenido> bl = new LinkedList<BloqueContenido>();
+		bl.add(b1);
+		bl.add(b2);
+		bl.add(b3);
+		Curso c1 = new Curso("c1", "c1", bl, 2, 3, "Español");
+		Curso c2 = new Curso("c2", "c2", bl, 2, 4, "Español");
+		Curso c3 = new Curso("c2", "c2", bl, 2, 6, "Español");	
+		List<Curso> cursosOnline = new LinkedList<Curso>();
+		cursosOnline.add(c1);
+		cursosOnline.add(c2);
+		cursosOnline.add(c3);
+		
+		List<Curso> ordenLikes = Controlador.INSTANCE.getCursosEnOrdenLikes(cursosOnline);
+		assertTrue(c3.equals(ordenLikes.get(0)));
+		assertTrue(c2.equals(ordenLikes.get(1)));
+		assertTrue(c1.equals(ordenLikes.get(2)));
+	}
+	
+	@Test
+	void testCursosEnOrdenDescargas() {
+		
+		BloqueContenido b1 = new BloqueContenido();
+		BloqueContenido b2 = new BloqueContenido();
+		BloqueContenido b3 = new BloqueContenido();
+		List<BloqueContenido> bl = new LinkedList<BloqueContenido>();
+		bl.add(b1);
+		bl.add(b2);
+		bl.add(b3);
+		Curso c1 = new Curso("c1", "c1", bl, 4, 3, "Español");
+		Curso c2 = new Curso("c2", "c2", bl, 5, 4, "Español");
+		Curso c3 = new Curso("c2", "c2", bl, 2, 6, "Español");	
+		List<Curso> cursosOnline = new LinkedList<Curso>();
+		cursosOnline.add(c1);
+		cursosOnline.add(c2);
+		cursosOnline.add(c3);
+		
+		List<Curso> ordenDescargas = Controlador.INSTANCE.getCursosEnOrdenDescargas(cursosOnline);
+		assertTrue(c2.equals(ordenDescargas.get(0)));
+		assertTrue(c1.equals(ordenDescargas.get(1)));
+		assertTrue(c3.equals(ordenDescargas.get(2)));
+	}
+
+}
